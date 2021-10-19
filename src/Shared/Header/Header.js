68611583acf97af08123ca8efda78e673c425acc
hrkanon <habibur3753@gmail.com>
2021-10-19 23:04:45 +0600

    finalagain

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -18,39 +18,48 @@ const Header = () => {
         sticky="top"
       >
         <Container>
-          <Navbar.Brand>
-            <img className="w-50" src={logo} alt="" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto text-center">
-              <Nav.Link className="" as={HashLink} to="/home">
-                Home
-              </Nav.Link>
-              <Nav.Link className="px-4" as={HashLink} to="/services">
-                Services
-              </Nav.Link>
-              <Nav.Link className="px-4" as={Link} to="/doctors">
-                doctors
-              </Nav.Link>
-              <Nav.Link as={HashLink} to="/review">
-                Reviews
-              </Nav.Link>
-              <Nav.Link className="px-4" as={Link} to="/appointment">
-                Appointment
-              </Nav.Link>
-              {user.email ? (
-                <Nav.Link onClick={handleSignOut}>
-                  {" "}
-                  <span className="pe-3">{user.displayName}</span> Logout
-                </Nav.Link>
-              ) : (
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+          <div className="d-flex align-items-center">
+            <div>
+              <Navbar.Brand>
+                <img className="w-50" src={logo} alt="" />
+              </Navbar.Brand>
+            </div>
+            <div>
+              <Navbar.Toggle
+                className="ms-auto"
+                aria-controls="responsive-navbar-nav"
+              />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto text-center">
+                  <Nav.Link className="" as={HashLink} to="/home">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link className="px-4" as={HashLink} to="/services">
+                    Services
+                  </Nav.Link>
+                  <Nav.Link className="px-4" as={Link} to="/doctors">
+                    doctors
+                  </Nav.Link>
+                  <Nav.Link as={HashLink} to="/review">
+                    Reviews
+                  </Nav.Link>
+                  <Nav.Link className="px-4" as={Link} to="/appointment">
+                    Appointment
+                  </Nav.Link>
+                  {user.email ? (
+                    <Nav.Link onClick={handleSignOut}>
+                      {" "}
+                      <span className="pe-3">{user.displayName}</span> Logout
+                    </Nav.Link>
+                  ) : (
+                    <Nav.Link as={Link} to="/login">
+                      Login
+                    </Nav.Link>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+          </div>
         </Container>
       </Navbar>
     </div>
